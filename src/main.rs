29ada024bb4ddf5cadbe2869ed25b3d1a702a5f2
Sam Davies <sam@uown.co>
2024-05-16 13:18:07 +0100

    reverted back to web will try build it as server

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,57 @@
-//! Run with:
+//! This example demonstrates a simple app that fetches a list of dog breeds and displays a random dog.
 //!
-//! ```sh
-//! dx serve --platform fullstack
-//! ```
+//! The app uses the `use_signal` and `use_resource` hooks to manage state and fetch data from the Dog API.
+//! `use_resource` is basically an async version of use_memo - it will track dependencies between .await points
+//! and then restart the future if any of the dependencies change.
+//!
+//! You should generally throttle requests to an API - either client side or server side. This example doesn't do that
+//! since it's unlikely the user will rapidly cause new fetches, but it's something to keep in mind.
 
-#![allow(non_snake_case, unused)]
 use dioxus::prelude::*;
-use serde::{Deserialize, Serialize};
+
+fn main() {
+    launch(app);
+}
 
 fn app() -> Element {
-    let mut count = use_signal(|| 0);
-    let mut text = use_signal(|| "...".to_string());
-    let server_future = use_server_future(get_server_data)?;
+    // Breed is a signal that will be updated when the user clicks a breed in the list
+    // `deerhound` is just a default that we know will exist. We could also use a `None` instead
+    let mut breed = use_signal(|| "kelpie".to_string());
+
+    // Fetch the list of breeds from the Dog API
+    // Since there are no dependencies, this will never restart
 
     rsx! {
-        h1 { "High-Five counter: {count}" }
-        button { onclick: move |_| count += 1, "Up high!" }
-        button { onclick: move |_| count -= 1, "Down low!" }
-        button {
-            onclick: move |_| async move {
-                if let Ok(data) = get_server_data().await {
-                    println!("Client received: {}", data);
-                    text.set(data.clone());
-                    post_server_data(data).await.unwrap();
-                }
-            },
-            "Run a server function!"
+
+        h1 { "Select a dog breed!" }
+        div { height: "500px", display: "flex",
+            div { flex: 1, BreedPic { breed } }
         }
-        "Server said: {text}"
-        "{server_future.state():?}"
     }
 }
 
-#[server]
-async fn post_server_data(data: String) -> Result<(), ServerFnError> {
-    println!("Server received: {}", data);
+#[component]
+fn BreedPic(breed: Signal<String>) -> Element {
+    // This resource will restart whenever the breed changes
+    let mut fut = use_resource(move || async move {
+        #[derive(serde::Deserialize, Debug)]
+        struct DogApi {
+            message: String,
+        }
 
-    Ok(())
-}
+        reqwest::get(format!("https://dog.ceo/api/breed/{breed}/images/random"))
+            .await
+            .unwrap()
+            .json::<DogApi>()
+            .await
+    });
 
-#[server]
-async fn get_server_data() -> Result<String, ServerFnError> {
-    Ok(reqwest::get("https://httpbin.org/ip").await?.text().await?)
-}
-
-fn main() {
-    #[cfg(feature = "web")]
-    tracing_wasm::set_as_global_default();
-
-    #[cfg(feature = "server")]
-    tracing_subscriber::fmt::init();
-
-    launch(app);
+    match fut.read_unchecked().as_ref() {
+        Some(Ok(resp)) => rsx! {
+            button { onclick: move |_| fut.restart(), "Click to fetch another doggo" }
+            img { max_width: "500px", max_height: "500px", src: "{resp.message}" }
+        },
+        Some(Err(_)) => rsx! { "loading image failed" },
+        None => rsx! { "loading image..." },
+    }
 }
